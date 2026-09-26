819c9c0133c2f199d376d645368afd77b832c70f
thanhxuan2k5 <xuanng205@gmail.com>
2026-09-26 12:01:47 +0700

    change port to 7000 main

--- main.py
+++ main.py
@@ -113,5 +113,5 @@ def home(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=7000, reload=True)
 
